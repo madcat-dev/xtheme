2ab7aa0a4f927be6f7f0906f9ebf37a6b98b1030
Yura <virt780@gmail.com>
2023-10-18 16:55:16 +0300

    fix

---
+++ templates/gtk-theme/FlatColor/install.sh
@@ -0,0 +1,22 @@
+# GTK theme 'FlatColor' installer
+#     Module from 'xtheme'
+CACHE="$HOME/.themes"
+TEMPLATE="$(dirname "$BASH_SOURCE")"
+DESTINATION="${DESTINATION:-$CACHE}"
+
+mkdir -p "$DESTINATION"				2>/dev/null
+rm   -rf "$DESTINATION/FlatColor"	2>/dev/null
+tar -xzf "$TEMPLATE/FlatColor.tar.gz" -C "$DESTINATION/" || return 1
+
+apply "$DESTINATION/FlatColor/gtk-2.0/gtkrc.base" \
+    "$DESTINATION/FlatColor/gtk-2.0/gtkrc" || return 1
+
+apply "$DESTINATION/FlatColor/gtk-3.0/gtk.css.base" \
+    "$DESTINATION/FlatColor/gtk-3.0/gtk.css" || return 1
+
+apply "$DESTINATION/FlatColor/gtk-3.20/gtk.css.base" \
+    "$DESTINATION/FlatColor/gtk-3.20/gtk.css" || return 1
+
+if [ ! -e "$DESTINATION/XTheme" ]; then
+    ln -s "$DESTINATION/FlatColor" "$DESTINATION/XTheme" || return 1
+fi
